6af4017883c06ada670840fef68d69978b16e5f4
edmundoandrade <edmundo@lia.ufc.br>
2014-12-13 21:45:56 -0200

    Test code refactoring.

--- src/test/java/edworld/pdfreader4humans/PDFReaderTest.java
+++ src/test/java/edworld/pdfreader4humans/PDFReaderTest.java
@@ -49,33 +49,7 @@ public class PDFReaderTest {
 				graphics.clearRect(0, 0, image.getWidth(), image.getHeight());
 				graphics.scale(scaling, scaling);
 				for (Component component : firstLevel)
-					if (component.toString().startsWith("box")) {
-						graphics.setColor(new Color(100, 100, 0));
-						graphics.fillRect(Math.round(component.getFromX()), Math.round(component.getFromY()), Math.round(component.getWidth()), Math.round(component.getHeight()));
-						graphics.setColor(Color.RED);
-						graphics.drawRect(Math.round(component.getFromX()), Math.round(component.getFromY()), Math.round(component.getWidth()), Math.round(component.getHeight()));
-						graphics.setColor(Color.WHITE);
-					} else if (component.toString().startsWith("group")) {
-						graphics.setColor(Color.CYAN);
-						graphics.drawRect(Math.round(component.getFromX()), Math.round(component.getFromY()), Math.round(component.getWidth()), Math.round(component.getHeight()));
-						graphics.setColor(Color.WHITE);
-					}
-				for (Component component : firstLevel)
-					if (component.toString().startsWith("line"))
-						graphics.drawLine(Math.round(component.getFromX()), Math.round(component.getFromY()), Math.round(component.getToX()), Math.round(component.getToY()));
-					else if (component.toString().startsWith("rect"))
-						graphics.drawRect(Math.round(component.getFromX()), Math.round(component.getFromY()), Math.round(component.getWidth()), Math.round(component.getHeight()));
-				for (Component component : firstLevel)
-					if (component.toString().startsWith("margin")) {
-						graphics.setColor(Color.YELLOW);
-						graphics.drawRect(Math.round(component.getFromX()), Math.round(component.getFromY()), Math.round(component.getWidth()), Math.round(component.getHeight()));
-						graphics.setColor(Color.WHITE);
-					}
-				for (Component component : firstLevel)
-					if (component instanceof TextComponent) {
-						graphics.setFont(font((TextComponent) component));
-						graphics.drawString(((TextComponent) component).getText(), component.getFromX(), component.getToY());
-					}
+					draw(component, graphics);
 				ImageIO.write(image, "png", new File("target/output.png"));
 				graphics.dispose();
 			} finally {
@@ -88,12 +62,39 @@ public class PDFReaderTest {
 		Assert.assertEquals(31, firstLevel.size());
 	}
 
+	private void draw(Component component, Graphics2D graphics) {
+		for (Component child : component.getChildren())
+			draw(child, graphics);
+		if (component.toString().startsWith("box")) {
+			graphics.setColor(new Color(100, 100, 0));
+			// graphics.fillRect(Math.round(component.getFromX()), Math.round(component.getFromY()), Math.round(component.getWidth()), Math.round(component.getHeight()));
+			graphics.setColor(Color.RED);
+			graphics.drawRect(Math.round(component.getFromX()), Math.round(component.getFromY()), Math.round(component.getWidth()), Math.round(component.getHeight()));
+			graphics.setColor(Color.WHITE);
+		} else if (component.toString().startsWith("group")) {
+			graphics.setColor(Color.CYAN);
+			graphics.drawRect(Math.round(component.getFromX()), Math.round(component.getFromY()), Math.round(component.getWidth()), Math.round(component.getHeight()));
+			graphics.setColor(Color.WHITE);
+		} else if (component.toString().startsWith("line"))
+			graphics.drawLine(Math.round(component.getFromX()), Math.round(component.getFromY()), Math.round(component.getToX()), Math.round(component.getToY()));
+		else if (component.toString().startsWith("rect"))
+			graphics.drawRect(Math.round(component.getFromX()), Math.round(component.getFromY()), Math.round(component.getWidth()), Math.round(component.getHeight()));
+		else if (component.toString().startsWith("margin")) {
+			graphics.setColor(Color.YELLOW);
+			graphics.drawRect(Math.round(component.getFromX()), Math.round(component.getFromY()), Math.round(component.getWidth()), Math.round(component.getHeight()));
+			graphics.setColor(Color.WHITE);
+		} else if (component instanceof TextComponent) {
+			graphics.setFont(font((TextComponent) component));
+			graphics.drawString(((TextComponent) component).getText(), component.getFromX(), component.getToY());
+		}
+	}
+
 	private Font font(TextComponent component) {
 		String key = component.getFontName() + ":" + component.getFontSize();
 		Font font = fonts.get(key);
 		if (font == null) {
 			String name = component.getFontName().contains("Times") ? "TimesRoman" : "Dialog";
-			int style = component.getFontName().contains("Bold") ? Font.BOLD : Font.PLAIN;
+			int style = component.getFontName().contains("Bold") ? Font.BOLD : (component.getFontName().contains("Italic") ? Font.ITALIC : Font.PLAIN);
 			font = new Font(name, style, (int) component.getFontSize());
 			fonts.put(key, font);
 		}
